b748c71adcb8b37a8d9566d2ec540be63031df01
Vinay Yadav <niovinay@gmail.com>
2021-05-14 14:00:52 +0530

    Update MainActivity.java
    Fetching useful_links collection from firestore

--- app/src/main/java/com/example/covidinfo/MainActivity.java
+++ app/src/main/java/com/example/covidinfo/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.covidinfo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupFirestore();
+    }
+
+    void setupFirestore() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //
+        db.collection("useful_links")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
     public void newsGrid(View view) {
